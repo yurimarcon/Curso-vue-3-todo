6d2ec549af843bf716fdd3e61eb43eae43cbc6b9
Yuri Marcon <yuri.erik.oliveira@gmail.com>
2023-11-12 12:53:08 -0300

    Estilizando o menu da aplicação

--- src/layouts/default/View.vue
+++ src/layouts/default/View.vue
@@ -1,8 +1,40 @@
 <template>
   <v-app id="inspire">
     <v-navigation-drawer v-model="drawer">
-      <router-link to="/">Go to Home</router-link>
-      <router-link to="/about">Go to About</router-link>
+      <v-list>
+          <v-list-item
+            prepend-avatar="https://github.com/yurimarcon/avatars/blob/main/Colored/ToyFaces_Colored_BG_29.jpg?raw=true"
+            title="Yuri Marcon"
+            subtitle="Todo List"
+          >
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list
+          :lines="false"
+          density="compact"
+          nav
+        >
+        <router-link
+        v-for="(item, i) in items"
+        :key="i"
+        :to="item.to"
+        >
+          <v-list-item
+            
+            :value="item"
+            color="primary"
+          >
+            <template v-slot:prepend>
+              <v-icon :icon="item.icon"></v-icon>
+            </template>
+
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item>
+        </router-link>
+        </v-list>
     </v-navigation-drawer>
 
     <v-app-bar>
@@ -20,11 +52,16 @@
 <script setup>
   import { ref } from 'vue'
 
-  const drawer = ref(null)
+  const drawer = ref(null);
+
+  const items = [
+    { text: 'Home', icon: 'mdi-folder', to: "/" },
+    { text: 'About', icon: 'mdi-account-multiple', to: "/about" }
+  ];
 </script>
 
-<script>
-  export default {
-    data: () => ({ drawer: null }),
+<style scoped>
+  a{
+    text-decoration: none;
   }
-</script>
+</style>
